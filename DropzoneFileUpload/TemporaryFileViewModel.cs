148cfeff0dd4ee4f54eb77c74ac0e9f3061d7e98
Huan <hhoangvan@pentalog.fr>
2014-08-22 17:20:06 +0700

    Dont need a form

---
+++ DropzoneFileUpload/TemporaryFileViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DropzoneFileUpload
+{
+    public class TemporaryFileViewModel
+    {
+        public string FileName { get; set; }
+        public int FileSize { get; set; }
+        public string ContentType { get; set; }
+        public string Path { get; set; }
+    }
+}
